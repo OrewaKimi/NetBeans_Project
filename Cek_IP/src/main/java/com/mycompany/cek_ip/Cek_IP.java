765eb87444ae4a1c2fbb6afcaec78cf6e146bdf5
Kimi Maulana <kimi93.com@gamil.com>
2025-03-12 13:57:56 +0700

    first commit

---
+++ Cek_IP/src/main/java/com/mycompany/cek_ip/Cek_IP.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cek_ip;
+
+/**
+ *
+ * @author kimi9
+ */
+public class Cek_IP {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
